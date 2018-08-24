1d9b1a703ee5965b8b99e2a0668a1cb913c7d54c
Epineda312 <epineda773@protonmail.com>
2018-08-24 01:34:28 -0700

    Update Program.cs

--- SoccerStats/SoccerStats/Program.cs
+++ SoccerStats/SoccerStats/Program.cs
@@ -14,18 +14,32 @@ namespace SoccerStats
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
-            var fileContents = ReadFile(FileName);
-            Console.WriteLine(fileContents);
-
+            var fileContents = ReadSoccerResults(fileName);
         }
 
-            public static string ReadFile(string FileName)
+
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
             {
-                using (var reader = new StreamReader(FileName))
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static List<string[]> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<string[]>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
                 {
-                    return reader.ReadToEnd();
+                    string[] values = line.Split(',');
+                    soccerResults.Add(values);
                 }
             }
+            return soccerResults;
+        }
     }
 }
-
